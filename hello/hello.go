e68e6a10f263003228260aec6bcd6df14206f484
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2025-01-28 07:06:39 -0500

    Add comments and Pi

--- hello/hello.go
+++ hello/hello.go
@@ -1,12 +1,16 @@
-package main
+package main // package main is special, it defines a standalone executable program
 
+// The factored import statement allows multiple packages to be imported in a single import declaration
 import (
-	"fmt"
+	"fmt" // fmt is a standard library package that provides formatted I/O
+	"math"
 
-	"rsc.io/quote"
+	"rsc.io/quote" // quote is a custom package that provides a function that returns a string
 )
 
 func main() {
 	fmt.Println("Hello, World!")
 	fmt.Println(quote.Go())
+	fmt.Println(math.Pi) // math is a standard library package that provides basic constants and mathematical functions
+	// Pi is an exported constant in the math package
 }
