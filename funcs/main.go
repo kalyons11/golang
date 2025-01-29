978bf16b311f321499a563966d5c719a533564ec
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2025-01-29 07:07:57 -0500

    Add func

--- funcs/main.go
+++ funcs/main.go
@@ -3,9 +3,17 @@ package main
 import "fmt"
 
 func main() {
-    demo_funcs()
+	demo_funcs()
+	fmt.Println(add(1, 2))
+	fmt.Println(add(3, 4))
 }
 
 func demo_funcs() {
-    fmt.Println("Hello from demo_funcs")
+	fmt.Println("Hello from demo_funcs")
+}
+
+// Variable type comes after the variable name
+// If multiple variables have the same type, you can write it once at the end
+func add(a, b int) int {
+	return a + b
 }
