5ad642caf9441b9f5267d82c5bbf7edfe627901e
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2025-01-29 07:10:16 -0500

    Add named return values

--- funcs/main.go
+++ funcs/main.go
@@ -6,6 +6,10 @@ func main() {
 	demo_funcs()
 	fmt.Println(add(1, 2))
 	fmt.Println(add(3, 4))
+	fmt.Println(swap(1, 2))
+	fmt.Println(swap(3, 4))
+	fmt.Println(swap2(1, 2))
+	fmt.Println(swap2(3, 4))
 }
 
 func demo_funcs() {
@@ -16,4 +20,16 @@ func demo_funcs() {
 // If multiple variables have the same type, you can write it once at the end
 func add(a, b int) int {
 	return a + b
+}
+
+// We can return multiple values
+func swap(a, b int) (int, int) {
+	return b, a
+}
+
+// We can use named return values
+func swap2(a, b int) (x, y int) {
+	x = b
+	y = a
+	return
 }
