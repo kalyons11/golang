36d99ba2413662b3e05a6f107c54b0562c6ef376
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2025-02-12 07:10:16 -0500

    Show pointers to structs

--- structs/main.go
+++ structs/main.go
@@ -15,4 +15,12 @@ type Vertex struct {
 func demo_structs() {
 	fmt.Println("Hello from demo_structs")
 	fmt.Println(Vertex{1, 2})
+
+	v := Vertex{1, 2}
+	v.X = 4
+	fmt.Println(v.X) // 4
+
+	p := &v
+	p.X = 1e9      // easier than (*p).X = 1e9
+	fmt.Println(v) // {1000000000 2}
 }
