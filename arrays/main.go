8e800a78bc7ec263a08f0a08fc778ba209104d6c
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2025-02-13 07:09:46 -0500

    Add notes for slice defaults

--- arrays/main.go
+++ arrays/main.go
@@ -58,4 +58,15 @@ func show_arrays() {
 		{13, true},
 	}
 	fmt.Println(list)
+
+	// Slice defaults
+	// When slicing, you may omit the high or low bounds to use their defaults instead.
+	// The default is zero for the low bound and the length of the slice for the high bound.
+	// For the array
+	// var a [10]int
+	// these slice expressions are equivalent:
+	// a[0:10]
+	// a[:10]
+	// a[0:]
+	// a[:]
 }
