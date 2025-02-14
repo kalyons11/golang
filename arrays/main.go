084e193a21473b55a5630ed64ab8dc828e7ec9a7
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2025-02-14 07:10:28 -0500

    Add slice length and capacity examples

--- arrays/main.go
+++ arrays/main.go
@@ -80,4 +80,30 @@ func show_arrays() {
 
 	test = test[1:]
 	fmt.Println(test)
+
+	describe_slices()
+}
+
+// The length of a slice is the number of elements it contains
+// The capacity of a slice is the number of elements in the underlying array,
+// counting from the first element in the slice
+func print_slice(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+}
+
+func describe_slices() {
+	s := []int{2, 3, 5, 7, 11, 13}
+	print_slice(s)
+
+	// Slice the slice to give it zero length
+	s = s[:0]
+	print_slice(s)
+
+	// Extend its length
+	s = s[:4]
+	print_slice(s)
+
+	// Drop its first two values
+	s = s[2:]
+	print_slice(s)
 }
