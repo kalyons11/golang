ea29f049260dcea03260f57f058f65150579398f
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2025-02-15 07:26:07 -0500

    Add func usages as arguments

--- funcs/main.go
+++ funcs/main.go
@@ -5,6 +5,23 @@ import "fmt"
 // We can use var to declare variables
 var c, python, java bool
 
+// We can pass functions as arguments
+// We can return functions
+// We can assign functions to variables
+// We can use functions as values
+// We can use functions as types
+// We can use functions as structs
+// We can use functions as fields in structs
+// We can use functions as return values in functions
+// We can use functions as arguments in functions
+// We can use functions as elements in arrays
+// We can use functions as elements in slices
+// We can use functions as elements in maps
+// We can use functions as elements in channels
+func compute(fn func(float64, float64) float64) float64 {
+	return fn(3, 4)
+}
+
 func main() {
 	demo_funcs()
 	fmt.Println(add(1, 2))
@@ -25,6 +42,16 @@ func main() {
 	// It is only available inside functions
 	k := 3
 	fmt.Println(k)
+
+	hypot := func(x, y float64) float64 {
+		return x + y
+	}
+	fmt.Println(hypot(5, 12))
+
+	fmt.Println(compute(hypot)) // 7
+	fmt.Println(compute(func(x, y float64) float64 {
+		return x * y
+	})) // 12
 }
 
 func demo_funcs() {
